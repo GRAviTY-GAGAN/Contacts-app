aa92d015b46ee701fcb4355d87c0ff00bb408b83
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-05 12:36:05 +0530

    Get all data from firebase

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -11,7 +11,9 @@ import {
 export default (state, action) => {
   switch (action.type) {
     case SET_CONTACT:
-      return action.payload == null ? {...state, contacts: []} : {...state, contacts: action.payload}
+      return action.payload == null
+       ? {...state, contacts: []}
+       : {...state, contacts: action.payload}
     case SET_LOADING:
       return {...state, isLoading: action.payload}
     case CONTACT_TO_UPDATE:
