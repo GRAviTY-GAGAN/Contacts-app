fbadaa2d0fd1efbc25f2f8df071a5838f8c5f10a
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-05 15:00:08 +0530

    adding configuration of firebase

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,14 @@
-//TODO: add firebase configuration and export it
+//TODO:DONE add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyBwIC4OMH4m1gaGr0b5-QQ1N56jCZHgy1Q",
+  authDomain: "mygitapp-299ee.firebaseapp.com",
+  databaseURL: "https://mygitapp-299ee-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-299ee",
+  storageBucket: "mygitapp-299ee.appspot.com",
+  messagingSenderId: "206213713014",
+  appId: "1:206213713014:web:358734131c38d0d8a42357",
+  measurementId: "G-7HC8W5K1LL"
+};
 
 //image configuration
 export const imageConfig = {
