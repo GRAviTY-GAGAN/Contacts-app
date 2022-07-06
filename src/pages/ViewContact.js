234a0d180c6ba0ef403d0b8805d8b12408f8842a
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-07-06 12:35:13 +0530

    Update

--- src/pages/ViewContact.js
+++ src/pages/ViewContact.js
@@ -53,7 +53,7 @@ const ViewContact = () => {
                 className="btn btn-primary btn-block"
                 target="_blank"
                 //TODO: add google maps
-                href={`https://maps.google.com/?=${contact?.address}`}
+                href={`https://www.google.com/maps/place/${contact?.address}`}
               >
                 <FaMapMarkerAlt className="icon mr-2" />
                 {contact?.address}
